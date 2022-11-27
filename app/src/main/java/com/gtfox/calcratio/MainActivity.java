e24efefb01768588bd6c62b9549986d0d93342e4
gtfox <gt-fox@mail.ru>
2022-11-27 04:34:45 +0300

    test

--- app/src/main/java/com/gtfox/calcratio/MainActivity.java
+++ app/src/main/java/com/gtfox/calcratio/MainActivity.java
@@ -1,11 +1,15 @@
 package com.gtfox.calcratio;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    static {
+        AppCompatDelegate.setDefaultNightMode(
+                AppCompatDelegate.MODE_NIGHT_YES);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
